c0c501f3374cda179d0f96a7580990f3ba7c0b96
anna-lopes <161318423+anna-lopes@users.noreply.github.com>
2024-09-02 11:11:24 -0300

    Add files via upload

--- src/components/ImagemDiversas.vue
+++ src/components/ImagemDiversas.vue
@@ -1,12 +1,12 @@
 <template>
-   <div>
+  <div>
     <h1>Imagem Diversas</h1>
     <!-- Conteúdo do componente -->
-    <img class="" src="@/assets/img/foto1.jpg" alt="" width="">
-    <img class="" src="@/assets/img/foto2.jpg" alt="" width="">
-    <img class="" src="@/assets/img/foto3.jpg" alt="" width="">
-    <img class="" src="@/assets/img/foto4.jpg" alt="" width="">
-
+    <img v-lazy="require('@/assets/your-image.jpg')" alt="Descrição da Imagem">
+    <img v-lazy="require('@/assets/img/foto1.jpg')" alt="Descrição da Imagem 1">
+    <img v-lazy="require('@/assets/img/foto2.jpg')" alt="Descrição da Imagem 2">
+    <img v-lazy="require('@/assets/img/foto3.jpg')" alt="Descrição da Imagem 3">
+    <img v-lazy="require('@/assets/img/foto4.jpg')" alt="Descrição da Imagem 4">
   </div>
 </template>
 
@@ -19,7 +19,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+/* Adicione estilos aqui se necessário */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+// import { createApp } from 'vue';
+// import App from './App.vue';
+// import router from './router';
+
+// const app = createApp(App);
+// app.use(router);
+// app.mount('#app');
+
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import { Lazyload } from 'vue-lazyload';
 
 const app = createApp(App);
-app.use(router);
+app.use(Lazyload);
 app.mount('#app');
